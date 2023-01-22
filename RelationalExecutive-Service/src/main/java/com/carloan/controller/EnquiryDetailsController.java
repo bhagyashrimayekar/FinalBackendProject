741dfbadd2a5adf33441118d17760ebe6ced0217
ShubhamMane123 <maneshubham.ms@gmail.com>
2023-01-22 10:23:55 +0530

    Enquiry Details Added

---
+++ RelationalExecutive-Service/src/main/java/com/carloan/controller/EnquiryDetailsController.java
@@ -0,0 +1,41 @@
+package com.carloan.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carloan.model.EnquiryDetails;
+import com.carloan.service.EnquiryDetailsService;
+
+@RestController
+@RequestMapping("/enquiryDetails")
+public class EnquiryDetailsController {
+	@Autowired
+	private EnquiryDetailsService service;
+
+	@PostMapping("/registerEnquiry")
+	public EnquiryDetails registerEnquiry(@RequestBody EnquiryDetails enquiryDetails) {
+		return service.registerEnquiry(enquiryDetails);
+
+	}
+
+	@GetMapping("/getAllEnquiryData")
+	public List<EnquiryDetails> getAllEnquiryData() {
+
+		List<EnquiryDetails> el = service.getAllEnquiryData();
+		return el;
+
+	}
+
+	@GetMapping("/findAllByCreatedDate/{createdDate}")
+	public List<EnquiryDetails> findAllByCreatedDate(@PathVariable String createdDate) {
+		List<EnquiryDetails> eList=service.findAllByCreatedDate(createdDate);
+		return eList;
+	}
+}
--- RelationalExecutive-Service/src/main/java/com/carloan/model/AllPersonalDocs.java
+++ RelationalExecutive-Service/src/main/java/com/carloan/model/AllPersonalDocs.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -12,17 +13,79 @@ public class AllPersonalDocs {
 	@Id
 	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	public int dID;
-    public byte addressProof;
-    public byte panCard;
-    public byte incomeTaxReturn;
-    public byte aadharCard;
-    public byte photo;
-    public byte signature;
+	@Lob
+    public byte[] addressProof;
+	@Lob
+    public byte[] panCard;
+	@Lob
+    public byte[] incomeTaxReturn;
+	@Lob
+    public byte[] aadharCard;
+	@Lob
+    public byte[] photo;
+	@Lob
+    public byte[] signature;
     //public byte thumb;
-    public byte bankCheque;
-    public byte salarySlip;
+	@Lob
+    public byte[] bankCheque;
+	@Lob
+    public byte[] salarySlip;
     
-    @ManyToOne
+    public int getdID() {
+		return dID;
+	}
+	public void setdID(int dID) {
+		this.dID = dID;
+	}
+	public byte[] getAddressProof() {
+		return addressProof;
+	}
+	public void setAddressProof(byte[] addressProof) {
+		this.addressProof = addressProof;
+	}
+	public byte[] getPanCard() {
+		return panCard;
+	}
+	public void setPanCard(byte[] panCard) {
+		this.panCard = panCard;
+	}
+	public byte[] getIncomeTaxReturn() {
+		return incomeTaxReturn;
+	}
+	public void setIncomeTaxReturn(byte[] incomeTaxReturn) {
+		this.incomeTaxReturn = incomeTaxReturn;
+	}
+	public byte[] getAadharCard() {
+		return aadharCard;
+	}
+	public void setAadharCard(byte[] aadharCard) {
+		this.aadharCard = aadharCard;
+	}
+	public byte[] getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	public byte[] getSignature() {
+		return signature;
+	}
+	public void setSignature(byte[] signature) {
+		this.signature = signature;
+	}
+	public byte[] getBankCheque() {
+		return bankCheque;
+	}
+	public void setBankCheque(byte[] bankCheque) {
+		this.bankCheque = bankCheque;
+	}
+	public byte[] getSalarySlip() {
+		return salarySlip;
+	}
+	public void setSalarySlip(byte[] salarySlip) {
+		this.salarySlip = salarySlip;
+	}
+	@ManyToOne
     public CustomerDetails customerDetails;
     
 	public CustomerDetails getCustomerDetails() {
@@ -31,59 +94,5 @@ public class AllPersonalDocs {
 	public void setCustomerDetails(CustomerDetails customerDetails) {
 		this.customerDetails = customerDetails;
 	}
-	public int getdID() {
-		return dID;
-	}
-	public void setdID(int dID) {
-		this.dID = dID;
-	}
-	public byte getAddressProof() {
-		return addressProof;
-	}
-	public void setAddressProof(byte addressProof) {
-		this.addressProof = addressProof;
-	}
-	public byte getPanCard() {
-		return panCard;
-	}
-	public void setPanCard(byte panCard) {
-		this.panCard = panCard;
-	}
-	public byte getIncomeTaxReturn() {
-		return incomeTaxReturn;
-	}
-	public void setIncomeTaxReturn(byte incomeTaxReturn) {
-		this.incomeTaxReturn = incomeTaxReturn;
-	}
-	public byte getAadharCard() {
-		return aadharCard;
-	}
-	public void setAadharCard(byte aadharCard) {
-		this.aadharCard = aadharCard;
-	}
-	public byte getPhoto() {
-		return photo;
-	}
-	public void setPhoto(byte photo) {
-		this.photo = photo;
-	}
-	public byte getSignature() {
-		return signature;
-	}
-	public void setSignature(byte signature) {
-		this.signature = signature;
-	}
-	public byte getBankCheque() {
-		return bankCheque;
-	}
-	public void setBankCheque(byte bankCheque) {
-		this.bankCheque = bankCheque;
-	}
-	public byte getSalarySlip() {
-		return salarySlip;
-	}
-	public void setSalarySlip(byte salarySlip) {
-		this.salarySlip = salarySlip;
-	}
-
+	
 }
